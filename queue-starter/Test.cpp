52281f65882abafde4a2ee9b3e1dbded14eae427
Anjali Pare <anjali.pare@gmail.com>
2019-11-15 15:15:25 -0600

    dequeue tests

--- queue-starter/Test.cpp
+++ queue-starter/Test.cpp
@@ -16,7 +16,7 @@ void Test::isEmptyTest1()
 void Test::isEmptyTest2()
 {
   Queue q;
-  q.enqueue(5);
+  q.enqueue(1);
   if (q.isEmpty()==true)
   {
     std::cout<<"\nTest 2: Enqueing a value makes the Queue not empty: FAILED";
@@ -27,10 +27,10 @@ void Test::isEmptyTest2()
   }
 }
 
-void Test::isEmptyTest3()
+void Test::dequeueTest1()
 {
   Queue q;
-  q.enqueue(5);
+  q.enqueue(1);
   q.dequeue();
   if (q.isEmpty()==true)
   {
@@ -42,12 +42,45 @@ void Test::isEmptyTest3()
   }
 }
 
+void Test::dequeueTest2()
+{
+  Queue q;
+  q.enqueue(1);
+  q.enqueue(2);
+  q.enqueue(3);
+  q.dequeue();
+  if (q.peekFront()==2)
+  {
+    std::cout<<"\nTest 4: Enqueue 1, 2, 3 on empty queue then dequeue then peekFront returns 2: PASSED";
+  }
+  else
+  {
+    std::cout<<"\nTest 4: Enqueue 1, 2, 3 on empty queue then dequeue then peekFront returns 2: FAILED";
+  }
+}
+
+void Test::dequeueTest3()
+{
+  Queue q;
+  try
+  {
+    q.dequeue();
+    std::cout<<"\nTest 5: Dequeue on an empty queue returns an exception: FAILED";
+  }
+  catch(std::runtime_error &e)
+  {
+    std::cout<<"\nTest 5: Dequeue on an empty queue returns an exception: PASSED";
+  }
+}
+
 
 
 void Test::runTest()
 {
   isEmptyTest1();
   isEmptyTest2();
-  isEmptyTest3();
+  dequeueTest1();
+  dequeueTest2();
+  dequeueTest3();
   std::cout<<"\n";
 }
